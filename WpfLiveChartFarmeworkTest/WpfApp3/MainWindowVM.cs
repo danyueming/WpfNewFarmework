df191d98d3031d21af1a6ddd15b234e0f6b904c8
danyueming <923306868@qq.com>
2019-08-22 09:06:53 +0800

    first

---
+++ WpfLiveChartFarmeworkTest/WpfApp3/MainWindowVM.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp3
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+
+        private UserControl _content;
+        public UserControl Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                OnPropertyChanged("Content");
+            }
+        }
+
+
+        private bool _isMenuOpen;
+        public bool IsMenuOpen
+        {
+            get { return _isMenuOpen; }
+            set
+            {
+                _isMenuOpen = value;
+                OnPropertyChanged("IsMenuOpen");
+            }
+        }
+
+        private string _criteria;
+
+        private IEnumerable<SampleGroupVm> _samples;
+        public IEnumerable<SampleGroupVm> Samples
+        {
+            get { return _samples; }
+            set
+            {
+                _samples = value;
+                OnPropertyChanged("Samples");
+            }
+        }
+        private readonly IEnumerable<SampleGroupVm> _dataSource;
+
+        public MainWindowVM()
+        {
+            _dataSource = new[]
+            {
+                new SampleGroupVm
+                {
+                    Name = "Customizing",
+                    Items = new[]
+                    {
+                         new SampleVm("Series", typeof(MyTestControl)),
+                          new SampleVm("Energy", typeof(MySecond))
+
+                    }
+                }
+            };
+
+            _samples = _dataSource;
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class IsNulLConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfLiveChartFarmeworkTest/WpfApp3/SampleVm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class SampleVm
+    {
+        public SampleVm()
+        {
+            Id = _idCount++;
+            Tags = string.Empty;
+        }
+
+        public SampleVm(string title, Type content, string tags = "")
+        {
+            Id = _idCount++;
+            Title = title;
+            Content = content;
+            Tags = tags;
+        }
+
+        private static int _idCount;
+
+        public int Id { get; private set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public Type Content { get; set; }
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public string Tags { get; set; }
+
+
+
+
+    }
+}
